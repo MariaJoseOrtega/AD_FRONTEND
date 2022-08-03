d575e77d47cc755e813effe39fefd2dab72446bb
Maria Jose <85966726+MariaJoseOrtega@users.noreply.github.com>
2022-08-02 21:57:14 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RolComponent } from './rol/rol.component';
+import { RolComponent } from './feature/rol/rol.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    RolComponent
   ],
   imports: [
     BrowserModule,
